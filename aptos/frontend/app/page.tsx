74b2be5636acea30fe9abab99043d378362fa612
0xPrimata <lucasvinzon@gmail.com>
2024-02-27 23:35:49 -0700

    fixes

--- aptos/frontend/app/page.tsx
+++ aptos/frontend/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-lg lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <p className="fixed cosmo left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
             AptosChat
           </p>
           <WalletSelector />
@@ -61,7 +61,7 @@ export default function Home() {
           </a>
 
           <a
-            href=""
+            href="https://github.com/movementlabsxyz/movement-dapp-workshop"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"

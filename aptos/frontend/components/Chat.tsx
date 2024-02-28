3fb593dca29ba1389991fc906016bdd4908c2b40
0xPrimata <lucasvinzon@gmail.com>
2024-02-28 10:42:43 -0700

    fixed hello message, hot reload

--- aptos/frontend/components/Chat.tsx
+++ aptos/frontend/components/Chat.tsx
@@ -21,7 +21,7 @@ const client = createSurfClient(new Aptos(new AptosConfig({ fullnode: "https://a
 const abi = { "address": "0x9a193de087aa158925b341c204d71b897d121f183a9ceddae7a8c2bb1c052a46", "name": "Chat", "friends": [], "exposed_functions": [{ "name": "create_chat_room", "visibility": "public", "is_entry": true, "is_view": false, "generic_type_params": [], "params": ["&signer"], "return": [] }, { "name": "get_messages", "visibility": "public", "is_entry": false, "is_view": true, "generic_type_params": [], "params": ["address"], "return": ["vector<0x5548665475c1807d19e3fc20bfb45cae242a14fce51ae1abb891ad06639c805e::Chat::Message>"] }, { "name": "post", "visibility": "public", "is_entry": true, "is_view": false, "generic_type_params": [], "params": ["&signer", "vector<u8>", "vector<u8>", "address"], "return": [] }, { "name": "post_with_ref", "visibility": "public", "is_entry": true, "is_view": false, "generic_type_params": [], "params": ["&signer", "vector<u8>", "address", "vector<u8>", "address"], "return": [] }], "structs": [{ "name": "ChatRoom", "is_native": false, "abilities": ["store", "key"], "generic_type_params": [], "fields": [{ "name": "messages", "type": "vector<0x5548665475c1807d19e3fc20bfb45cae242a14fce51ae1abb891ad06639c805e::Chat::Message>" }, { "name": "message_count", "type": "u64" }] }, { "name": "Message", "is_native": false, "abilities": ["copy", "store", "key"], "generic_type_params": [], "fields": [{ "name": "sender", "type": "address" }, { "name": "text", "type": "vector<u8>" }, { "name": "timestamp", "type": "u64" }, { "name": "ref_id", "type": "0x1::option::Option<address>" }, { "name": "metadata", "type": "vector<u8>" }] }] } as const;
 export default function Chat() {
     const [history, setHistory] = useState([] as Message[]);
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("hello");
     const { account } = useWallet();
 
     const {
@@ -46,6 +46,9 @@ export default function Chat() {
 
     useEffect(() => {
         getMessages();
+        const intervalId = setInterval(getMessages, 5000); 
+
+        return () => clearInterval(intervalId);
     }, [account?.address, submitResult, submitError, submitIsLoading])
 
 

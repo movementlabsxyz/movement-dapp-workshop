d7c56b481833777d4cfb1cbd6fd12b1070847b87
0xPrimata <lucasvinzon@gmail.com>
2024-02-29 18:09:19 -0700

    fix new aptos

--- sui/frontend/app/page.tsx
+++ sui/frontend/app/page.tsx
@@ -60,7 +60,7 @@ const queryClient = new QueryClient();
 
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
               <a
-                href="https://main.d3pm9dhwxzeo2g.amplifyapp.com/"
+                href="https://main.d2761w90g9sxb3.amplifyapp.com/"
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                 target="_blank"
                 rel="noopener noreferrer"
